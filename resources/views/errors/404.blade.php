3cd1a48a7a432ee8ca2c48d95730834521d1d113
thanhhhb <80967519+hobathanh@users.noreply.github.com>
2022-06-23 11:07:24 +0700

    update 403,404 (#47)

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,23 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>403</title>
-    <link rel="stylesheet" href="{{asset('admins/errors/403.css')}} ">
+    <title>404</title>
+    <link rel="stylesheet" href="{{asset('admins/errors/404.css')}} ">
 </head>
 <body>
 <div id="error-page">
     <div class="content">
-        <h1 class="header" data-text="403">403</h1>
-        <h2 data-text="ACCESS FORBIDDEN">ACCESS FORBIDDEN</h2>
+        <h1 class="header" data-text="404">404</h1>
+        <h2 data-text="PAGE NOT POUND">PAGE NOT POUND</h2>
         <p>
-            We are sorry, but you do not have access to this page or resoucre
+            The page you were looking for doesn't exist, Please go back to the
+            homepage now!
         </p>
 {{--        <div class="btns">--}}
 {{--            <a href="https://github.com/hobathanh">return home</a>--}}
 {{--        </div>--}}
     </div>
 </div>
-
 </body>
 </html>
 
