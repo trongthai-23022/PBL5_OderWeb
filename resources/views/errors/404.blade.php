712bbdeace788ea3c1d8971d0857201b2276561d
HO BA THANH <hobathanh201@gmail.com>
2022-06-24 00:29:41 +0700

    update errors view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,31 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>404</title>
-    <link rel="stylesheet" href="{{asset('admins/errors/404.css')}} ">
+
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>404 Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('admins/errors/404.css')}}" rel="stylesheet">
 </head>
+
+
 <body>
-<div id="error-page">
-    <div class="content">
-        <h1 class="header" data-text="404">404</h1>
-        <h2 data-text="PAGE NOT POUND">PAGE NOT POUND</h2>
-<<<<<<< HEAD
-=======
-        <p>
-
-            The page you were looking for doesn't exist, Please go back to the page now!
+<div class="d-flex align-items-center justify-content-center vh-100">
+    <div class="text-center">
+        <h1 class="display-1 fw-bold beri-color">404</h1>
+        <p class="fs-3"> <span class="text-danger beri-color">Opps!</span> Page not found.</p>
+        <p class="lead">
+            The page you’re looking for doesn’t exist.
         </p>
-        <div class="btns">
-            <a href="{{ url()->previous() }}">Go Back</a>
-        </div>
->>>>>>> 0ba20bf23c6e9ce8fb660df0afb38af220704726
-
+        <a href="{{ url()->previous() }}" class="btn btn-primary beri-bg-color">Go Home</a>
     </div>
 </div>
 </body>
-</html>
 
+
+</html>
